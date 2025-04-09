927ebd26b4b263630fab9e876ab9288889493e44
Pierre Mirouze <pierre.mrz13@gmail.com>
2025-04-09 10:28:34 +0200

    blink+uart raw freertos (no CMSIS)

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -29,7 +29,6 @@
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
-typedef StaticTask_t osStaticThreadDef_t;
 /* USER CODE BEGIN PTD */
 
 /* USER CODE END PTD */
@@ -48,35 +47,32 @@ typedef StaticTask_t osStaticThreadDef_t;
 /* USER CODE BEGIN Variables */
 
 /* USER CODE END Variables */
-/* Definitions for defaultTask */
-osThreadId_t defaultTaskHandle;
-const osThreadAttr_t defaultTask_attributes = {
-  .name = "defaultTask",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityNormal,
-};
-/* Definitions for blinkerTask */
-osThreadId_t blinkerTaskHandle;
-uint32_t blinkerTaskBuffer[ 128 ];
-osStaticThreadDef_t blinkerTaskControlBlock;
-const osThreadAttr_t blinkerTask_attributes = {
-  .name = "blinkerTask",
-  .cb_mem = &blinkerTaskControlBlock,
-  .cb_size = sizeof(blinkerTaskControlBlock),
-  .stack_mem = &blinkerTaskBuffer[0],
-  .stack_size = sizeof(blinkerTaskBuffer),
-  .priority = (osPriority_t) osPriorityAboveNormal,
-};
+osThreadId defaultTaskHandle;
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
 
 /* USER CODE END FunctionPrototypes */
 
-void StartDefaultTask(void *argument);
-void blinkerTask0(void *argument);
+void StartDefaultTask(void const * argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/* GetIdleTaskMemory prototype (linked to static allocation support) */
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize );
+
+/* USER CODE BEGIN GET_IDLE_TASK_MEMORY */
+static StaticTask_t xIdleTaskTCBBuffer;
+static StackType_t xIdleStack[configMINIMAL_STACK_SIZE];
+
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize )
+{
+  *ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer;
+  *ppxIdleTaskStackBuffer = &xIdleStack[0];
+  *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+  /* place for user code */
+}
+/* USER CODE END GET_IDLE_TASK_MEMORY */
 
 /**
   * @brief  FreeRTOS initialization
@@ -105,19 +101,13 @@ void MX_FREERTOS_Init(void) {
   /* USER CODE END RTOS_QUEUES */
 
   /* Create the thread(s) */
-  /* creation of defaultTask */
-  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
-
-  /* creation of blinkerTask */
-  blinkerTaskHandle = osThreadNew(blinkerTask0, NULL, &blinkerTask_attributes);
+  /* definition and creation of defaultTask */
+  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
+  defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
   /* USER CODE END RTOS_THREADS */
-
-  /* USER CODE BEGIN RTOS_EVENTS */
-  /* add events, ... */
-  /* USER CODE END RTOS_EVENTS */
 
 }
 
@@ -128,7 +118,7 @@ void MX_FREERTOS_Init(void) {
   * @retval None
   */
 /* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void *argument)
+void StartDefaultTask(void const * argument)
 {
   /* USER CODE BEGIN StartDefaultTask */
   /* Infinite loop */
@@ -137,26 +127,6 @@ void StartDefaultTask(void *argument)
     osDelay(1);
   }
   /* USER CODE END StartDefaultTask */
-}
-
-/* USER CODE BEGIN Header_blinkerTask0 */
-/**
-* @brief Function implementing the blinkerTask thread.
-* @param argument: Not used
-* @retval None
-*/
-/* USER CODE END Header_blinkerTask0 */
-void blinkerTask0(void *argument)
-{
-  /* USER CODE BEGIN blinkerTask0 */
-  /* Infinite loop */
-  for(;;)
-  {
-	HAL_GPIO_TogglePin(GPIOI, GPIO_PIN_1);
-	HAL_UART_Transmit(&huart1, (uint8_t *)"blink the led!\r\n", 18, HAL_MAX_DELAY);
-    osDelay(100);
-  }
-  /* USER CODE END blinkerTask0 */
 }
 
 /* Private application code --------------------------------------------------*/
