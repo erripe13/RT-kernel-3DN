c1156ba8503b4c622f1f255d35b98e6d001f24be
Pierre Mirouze <pierre.mrz13@gmail.com>
2025-04-09 09:04:01 +0200

    ioputchar

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -34,7 +34,10 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+int __io_putchar(int ch) {
+HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+return ch;
+}
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
